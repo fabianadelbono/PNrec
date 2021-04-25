715d096a5a5da06426da617f7678e5ae15c6b587
fabianadelbono <55921230+fabianadelbono@users.noreply.github.com>
2021-04-25 11:20:06 +0200

    Upload - 1. NEURON

---
+++ Code/1. NEURON/5.7/mod_func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "hocdec.h"
+#define IMPORT extern __declspec(dllimport)
+IMPORT int nrnmpi_myid, nrn_nobanner_;
+
+extern void _AXNODE_reg();
+
+void modl_reg(){
+	//nrn_mswindll_stdio(stdin, stdout, stderr);
+    if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+	fprintf(stderr, "Additional mechanisms from files\n");
+
+fprintf(stderr," AXNODE.mod");
+fprintf(stderr, "\n");
+    }
+_AXNODE_reg();
+}
